08a0ad05101a9a1c9512913467efd281b5f90582
DarthCorvidus <github@vm01.telton.de>
2019-11-15 21:45:23 +0100

    pnkrebuild rebuilds monthly, weekly and yearly entries from daily entries using cp -alv.

---
+++ include/local/RebuildJob/ArgvRebuild.py
@@ -0,0 +1,39 @@
+from operator import length_hint
+
+from include.lib.Argv.ArgModel import ArgModel
+from include.lib.Argv.ArgString import ArgString
+from include.lib.Argv.ArgvModel import ArgvModel
+
+
+class ArgvRebuild(ArgvModel):
+	__positional = None
+	__positionalNames = None
+	__named = None
+	def __init__(self):
+		self.__positional = []
+		self.__positionalNames = []
+		self.__positional.append(ArgString())
+		self.__positionalNames.append("backup")
+		self.__named = {}
+		self.__named["max"] = ArgString()
+
+	def getArgNames(self) -> list:
+		return self.__named.keys()
+
+	def getNamedArg(self, name: str) -> ArgModel:
+		return self.__named[name]
+
+	def getPositionalCount(self) -> int:
+		return len(self.__positional)
+
+	def getPositionalArg(self, i: int) -> ArgModel:
+		return self.__positional[i]
+
+	def getPositionalName(self, i: int) -> str:
+		pass
+
+	def getBoolean(self) -> list:
+		return ["weekly", "monthly", "yearly", "run"]
+
+	def run(self):
+		pass
---
+++ include/local/RebuildJob/RebuildJob.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+import subprocess
+
+import sys
+
+from include.lib.Argv import ArgvModel
+from include.lib.Argv.Argv import Argv
+from include.local.BackupEntries import BackupEntries
+from include.local.BackupEntry import BackupEntry
+from include.local.BackupEntryFilter import BackupEntryFilter
+from include.local.RebuildJob.ArgvRebuild import ArgvRebuild
+
+
+class RebuildJob():
+	__argv = None
+	__entries = None
+	__rebuild = []
+	__max = None
+	__count = 0
+	def __init__(self, argv:list):
+		self.__rebuild = []
+		argvModel = ArgvRebuild()
+		self.__argv = Argv(argvModel, argv)
+		if self.__argv.hasNamedValue("max"):
+			self.__max = int(self.__argv.getNamedValue("max"))
+		entries = BackupEntries.fromPath(self.__argv.getPositionalValue(0))
+		filter = BackupEntryFilter()
+		filter.setPeriods(["daily"])
+		self.__entries = entries.getFiltered(filter)
+		for i in range(self.__entries.getEntryCount()):
+			entry = self.__entries.getEntry(i)
+			self.__determineRebuild(entry)
+
+	def __determineRebuild(self, entry:BackupEntry):
+		weekly = entry.getAbsolutePath() + ".weekly"
+		monthly = entry.getAbsolutePath() + ".monthly"
+		yearly = entry.getAbsolutePath() + ".yearly"
+		periods = ["weekly", "monthly", "yearly"]
+		strftime = ["%w", "%d", "%m-%d"]
+		expected = ["0", "01", "01-01"]
+		for i in range(len(periods)):
+			self.__checkCondition(entry, periods[i], strftime[i], expected[i])
+
+	def __checkCondition(self, entry:BackupEntry, period, strftime, expected):
+		if(self.__max is not None and self.__max==self.__count):
+			return
+		path = entry.getAbsolutePath() + "."+period
+		if self.__argv.getBoolean(period) is True and os.path.isdir(path) is False and entry.getDate().strftime(strftime)==expected:
+			self.__rebuild.append(path)
+			self.__count = self.__count + 1
+
+	def __rebuildPeriods(self, path:str):
+		basename = os.path.basename(path)
+		datename = basename.split(".")[0]
+		print(datename)
+		src = os.path.dirname(path)+"/"+datename
+		tmp = os.path.dirname(path)+"/temp.rebuild"
+		dst = path
+		print("Source:    "+src)
+		print("Temporary: "+tmp)
+		print("Final:     "+dst)
+		if os.path.isdir(tmp):
+			print("Deleting "+tmp)
+			shutil.rmtree(tmp)
+		print("Copying "+src+" to "+tmp)
+		subprocess.run(["cp", src, tmp, "-al"])
+		print("Rename "+tmp+" to "+dst+"\n\n")
+		os.rename(tmp, dst)
+
+
+	def run(self):
+		for path in self.__rebuild:
+			print(path)
+		if self.__argv.getBoolean("run") is not True:
+			print("use --run to rebuild.")
+			return
+		for path in self.__rebuild:
+			self.__rebuildPeriods(path)
