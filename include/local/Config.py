cd123cc7a5b3c2d619ada49ca7539a9ee8d0ef5e
DarthCorvidus <github@vm01.telton.de>
2019-11-16 23:29:38 +0100

    ConfigParser doesn't support ini files without a section yet. Therefore there's a primitive parser, which represent Backup configurations.

---
+++ include/local/Config.py
@@ -0,0 +1,30 @@
+class Config:
+	__values = {}
+
+	def __init__(self, file):
+		self.__values = {}
+		handle = open(file, "r")
+		for line in handle:
+			self.__parse(line)
+		handle.close()
+		print(self.__values)
+	def __parse(self, line:str):
+		stripped = line.strip()
+		if len(stripped)==0:
+			return
+		if stripped[0] == "#":
+			return
+		split = stripped.split("=")
+		self.__values[split[0].strip()] = split[1].strip()
+
+	def getSource(self):
+		return self.__values["source"]
+
+	def getTarget(self):
+		return self.__values["target"]
+
+	def hasExclude(self):
+		return "exclude" in self.__values.keys()
+
+	def getExclude(self):
+		return self.__values["exclude"]
