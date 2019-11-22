34c6081a8d3df6f241811537d8379b48ba04bd87
DarthCorvidus <github@vm01.telton.de>
2019-11-22 17:31:46 +0100

    Added filter parameter subdir, which filters for entries that have a certain subdirectory.

--- include/local/BackupEntryFilter.py
+++ include/local/BackupEntryFilter.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 
 from include.local.BackupEntry import BackupEntry
@@ -9,7 +10,7 @@ class BackupEntryFilter:
 		self.__dateTo = None
 		self.__periods = None
 		self.__limit = None
-		pass
+		self.__subdir = None
 
 	def setFrom(self, dateFrom: datetime.date):
 		self.__dateFrom = dateFrom
@@ -29,12 +30,17 @@ class BackupEntryFilter:
 	def hasLimit(self) -> int:
 		return self.__limit is not None
 
+	def setSubdir(self, subdir:str):
+		self.__subdir = subdir
+
 	def filter(self, entry:BackupEntry) -> bool:
 		if self.filterTo(entry) is not True:
 			return False
 		if self.filterFrom(entry) is not True:
 			return False
 		if self.filterPeriod(entry) is not True:
+			return False
+		if self.filterSubdir(entry) is not True:
 			return False
 		return True
 
@@ -58,3 +64,9 @@ class BackupEntryFilter:
 		if entry.getPeriod() in self.__periods:
 			return True
 		return False
+
+	def filterSubdir(self, entry:BackupEntry) -> bool:
+		if self.__subdir is None:
+			return True
+		path = entry.getAbsolutePath()+"/"+self.__subdir
+		return os.path.exists(path)
